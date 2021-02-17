74d4976c9cceca6e3527d0951ddb98f4afda1543
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-17 13:20:53 +0530

    added bg img

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
 
 
 <body>
+<link rel="stylesheet" type="text/css" href="css.css" media="screen" />
+
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-	<div class="container">
+	<div class="container"><br><br><br>
 		<div class="row">
 			<div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
 				<div class="card my-5">
@@ -22,12 +24,12 @@
   						<fieldset>
   							 </div>
     							<div class="form-group">
-      								<label for="exampleInputEmail1">Email address</label>
+      								
       									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
       			
     							</div><br>
     							<div class="form-group">
-      								<label for="exampleInputPassword1">Password</label>
+      								
       								<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
     								</div><br>
     								<div class="text-center">
