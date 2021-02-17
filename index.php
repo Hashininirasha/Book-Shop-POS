5228f0e8e96e6554381622e3b4b0e7c771cb4e68
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-17 12:58:13 +0530

    added form

--- index.php
+++ index.php
@@ -18,7 +18,22 @@
 
 					 		Book Shop  
 					  </h4>
-
+					  <form>
+  						<fieldset>
+  							 </div>
+    							<div class="form-group">
+      								<label for="exampleInputEmail1">Email address</label>
+      									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      			
+    							</div><br>
+    							<div class="form-group">
+      								<label for="exampleInputPassword1">Password</label>
+      								<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    								</div><br>
+    								<div class="text-center">
+    								<button type="submit" class="btn btn-primary">Log In</button>
+  					</fieldset>
+					</form>
 				</div>
 
 
